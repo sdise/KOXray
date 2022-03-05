7e1df4cca0fb92a0dfd5da360577074fe65cc8f6
sdise <33455369+sdise@users.noreply.github.com>
2022-03-06 01:03:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 tor &
 /xray -config /xray.json &
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+wget -O xui.sh https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh &&
+bash xui.sh
